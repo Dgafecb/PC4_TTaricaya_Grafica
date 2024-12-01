75ac55917deb3f2d0201ef68bcb82e124d6b7b50
GaboYR <gabriel.yarleque.r@uni.pe>
2024-11-30 19:23:24 -0500

    Colisiones

--- River.tsx
+++ River.tsx
@@ -1,4 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="River" tilewidth="32" tileheight="32" tilecount="10626" columns="138">
  <image source="assets/images/Sunnyside_World_ASSET_PACK_V2.1/Sunnyside_World_ASSET_PACK_V2.1/Sunnyside_World_Assets/Sunnyside_World_ExampleScene.png" width="4434" height="2470"/>
+ <tile id="7713">
+  <objectgroup draworder="index" id="2">
+   <object id="12" name="Poste" x="0" y="0" width="32" height="32"/>
+   <object id="13" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
