ebf5fe749991dc0490e8617c0b1717b71efba9fc
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2024-12-04 12:40:06 -0500

    cambios

--- src/main.py
+++ src/main.py
@@ -1,6 +1,39 @@
-from libraries import *
+import pygame
+from settings import WIDTH, HEIGHT, FPS
+from player import Player
+from utils import load_tileset, draw_map
+from dialogue import DialogueBox  # Importa el cuadro de diálogo
+from pytmx import load_pygame
+from utils import load_story_from_json
+from turtles import Turtle
+import random
+from crab import Crab  # Importa la clase Crab
+from utils import  draw_score
+import time
+from power import Power  # Importa la clase Power
+from utils import  draw_powerup_info
+from zorro import Fox
+from egg import Egg
+import math
+import json
+from utils import mostrar_letrero_personalizado
+from buttons import Button
+from enemy import Enemy
+from utils import draw_image, draw_map_from_tmx, check_collision,check_collision_power
+from utils import init_objects
+from utils import generate_random_turtle,generate_random_fox,generate_random_enemy
+from settings import WIDTH, HEIGHT, FPS
+import os
+import sys
+from gif import GifBackground
+import subprocess
+import pygame_menu
+
+
 from utils import *
 from dialogue import DialogueBox
+import asyncio
+
 # Inicializa el mixer de Pygame
 pygame.mixer.init()
 
@@ -156,7 +189,7 @@ TEMP = {"estado_actual":10}
 gif_bg = GifBackground("../video.gif", (WIDTH, HEIGHT))
 
 
-def main():
+async def main():
     global following_turtle, score, time_left, start_time,start_time_dia,estado_actual, start_time_noche # Usamos la variable global para modificarla dentro del ciclo principal
     global foxes,enemies,eggs,crabs,egg_packs,player,boton_perros,boton_sirena,dialogue_box,turtles,powerups,tmx_map_dia,mapa_noche
     
@@ -691,7 +724,9 @@ def main():
                 pygame.mixer.music.play(loops=-1, start=0.0)
                 pass
         
+        
         pygame.display.flip()
+    await asyncio.sleep(0)
               
-if __name__ == "__main__":
-    main()
+
+asyncio.run(main())
